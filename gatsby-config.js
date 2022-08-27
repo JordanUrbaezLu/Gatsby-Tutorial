3dfa2de29d499bc9585e7af7eaf3d238c4ade7bb
Jordan Urbaez Lu <jordana.urbaez@gmail.com>
2022-08-27 18:00:44 -0400

    broken?

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 module.exports = {
   siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
     title: "My First Gatsby Site",
   },
   plugins: [
@@ -12,6 +13,7 @@ module.exports = {
         path: `${__dirname}/blog`,
       }
     },
-    `gatsby-plugin-mdx`,
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
   ],
 };
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,13 +1,13 @@
-import * as React from "react";
-import { graphql, Link } from "gatsby";
-import Layout from "../../components/layout";
-import Seo from "../../components/seo";
+import * as React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {
-        data.allMdx.nodes.map((node) => (
+        data.allMdx.nodes.map(node => (
           <article key={node.id}>
             <h2>
               <Link to={`/blog/${node.frontmatter.slug}`}>
@@ -15,16 +15,17 @@ const BlogPage = ({ data }) => {
               </Link>
             </h2>
             <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
           </article>
         ))
       }
     </Layout>
-  );
-};
+  )
+}
 
 export const query = graphql`
   query {
-    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
@@ -36,8 +37,8 @@ export const query = graphql`
       }
     }
   }
-`;
+`
 
-export const Head = () => <Seo title="My Blog Posts" />;
+export const Head = () => <Seo title="My Blog Posts" />
 
-export default BlogPage;
+export default BlogPage
